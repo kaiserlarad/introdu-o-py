a90daed603bc6600d28e03adcca586e512552c63
kaiserlarad <120047294+kaiserlarad@users.noreply.github.com>
2022-12-15 21:46:55 -0300

    Create a4.py

---
+++ Atividade 13.12/a4.py
@@ -0,0 +1,12 @@
+valor1= int(input('Digite o valor de X: '))
+valor2= int(input('Digite o valor de Y: '))
+
+troca1= ()
+troca2= ()
+
+troca1= valor1
+troca2= valor2
+valor1= troca2
+valor2= troca1
+
+print('1º valor: ',valor1,'\n2º valor: ',valor2)
